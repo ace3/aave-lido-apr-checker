0c2b9a7070247f6ebb9e8ba088af23ec620f3180
Ignasius Wahyudi <ignasius@usenobi.com>
2023-12-18 12:51:28 +0700

    Feat: add builder

--- index.js
+++ index.js
@@ -5,36 +5,62 @@ const RPC_URL = 'https://arbitrum.llamarpc.com'
 const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
 const markets = require('@bgd-labs/aave-address-book')
 const axios = require('axios')
+const Fastify = require('fastify')
 
-;(async () => {
-  // Run The Command Here
+const fastify = Fastify({
+  logger: true,
+})
 
-  const lidoResult = await axios.get(
-    'https://eth-api.lido.fi/v1/protocol/steth/apr/sma'
-  )
-  const lido = lidoResult.data.data.smaApr
-  const poolDataProviderContract = new UiPoolDataProvider({
-    uiPoolDataProviderAddress: markets.AaveV3Arbitrum.UI_POOL_DATA_PROVIDER,
-    provider,
-    chainId: ChainId.arbitrum_one,
-  })
+const fetchRatio = async () => {
+  try {
+    const lidoResult = await axios.get(
+      'https://eth-api.lido.fi/v1/protocol/steth/apr/sma'
+    )
+    const lido = lidoResult.data.data.smaApr
+    const poolDataProviderContract = new UiPoolDataProvider({
+      uiPoolDataProviderAddress: markets.AaveV3Arbitrum.UI_POOL_DATA_PROVIDER,
+      provider,
+      chainId: ChainId.arbitrum_one,
+    })
 
-  const result = await poolDataProviderContract.getReservesHumanized({
-    lendingPoolAddressProvider: markets.AaveV3Arbitrum.POOL_ADDRESSES_PROVIDER,
-  })
+    const result = await poolDataProviderContract.getReservesHumanized({
+      lendingPoolAddressProvider:
+        markets.AaveV3Arbitrum.POOL_ADDRESSES_PROVIDER,
+    })
 
-  const weth = result.reservesData.find((item) => item.symbol === 'WETH')
+    const weth = result.reservesData.find((item) => item.symbol === 'WETH')
 
-  const wethBorrowRate = (weth.variableBorrowRate / 1e27) * 100
-  const aaveBorrowRate = parseFloat(wethBorrowRate.toFixed(16))
-  const threshold = 0.2
-  const diff = lido - aaveBorrowRate
-  const obj = {
-    lido_apr: lido,
-    aave_borrow_rate: aaveBorrowRate,
-    difference: diff,
-    threshold: 0.2,
-    bad: lido - diff < threshold,
+    const wethBorrowRate = (weth.variableBorrowRate / 1e27) * 100
+    const aaveBorrowRate = parseFloat(wethBorrowRate.toFixed(16))
+    const threshold = 0.2
+    const diff = lido - aaveBorrowRate
+    const obj = {
+      status: true,
+      data: {
+        lido_apr: lido,
+        aave_borrow_rate: aaveBorrowRate,
+        difference: diff,
+        threshold: 0.2,
+        bad: lido - diff < threshold,
+      },
+    }
+    return obj
+  } catch (e) {
+    return {
+      status: false,
+      data: null,
+    }
   }
-  console.log(obj)
-})()
+}
+
+// Declare a route
+fastify.get('/', async function handler(request, reply) {
+  return await fetchRatio()
+})
+// Run the server!
+try {
+  fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
